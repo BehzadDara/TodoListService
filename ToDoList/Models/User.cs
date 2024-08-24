b381cef91e6d53c66e2f9fdd23bffc37d937aec4
behzad dara <behzad.dara.99@gmail.com>
2024-08-24 12:11:42 +0330

    Add project files.

---
+++ ToDoList/Models/User.cs
@@ -0,0 +1,5 @@
+﻿namespace TodoList.Models;
+
+public class User
+{
+}
---
+++ ToDoList/Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/", () => "Hello, World!");
+
+//app.UseAuthorization();
+
+app.Run();
